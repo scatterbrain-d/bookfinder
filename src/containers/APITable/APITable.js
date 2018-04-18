381358368815503e9e5a73f4a1b490c62afc4fe6
Andy Finnell <andy.finnell@gmail.com>
2018-04-18 20:30:54 +0000

    pagination

--- src/containers/APITable/APITable.js
+++ src/containers/APITable/APITable.js
@@ -7,41 +7,42 @@ class APITable extends Component {
   
   state = {
     results: [],
+    totalItems: 0,
     titleInput: '',
     authorInput: '',
     categoryInput: '',
-    startIndex: 1
+    page: 1
   }
   
   inputHandler = (event) => {
     this.setState({[event.target.name]: event.target.value});
   }
   
-  // Google Books API key
-  // key=AIzaSyBWeTQ4CaU31-FevfbyRtiThB_AOuzAz7g
+  
   
   searchHandler = () => {
     let params = '';
-    let maxAndStart = '&maxResults=20&startIndex=' + this.state.startIndex;
+    let maxAndStart = '&maxResults=20&startIndex=' + (1 + 20 * (this.state.page - 1));
     const APIkey = '&key=AIzaSyBWeTQ4CaU31-FevfbyRtiThB_AOuzAz7g';
-    const fields = '&fields=totalItems,items(id,volumeInfo(title,authors,publishedDate,categories,imageLinks(smallThumbnail),previewLink))'
+    const fields = '&fields=totalItems,items(id,volumeInfo(title,authors,publishedDate,categories,imageLinks(smallThumbnail),previewLink))';
     
     if (this.state.titleInput)
-      {params += 'intitle:' + this.state.titleInput + '&';
-      console.log(params);}
+      params += 'intitle:' + this.state.titleInput + '&';
     if (this.state.authorInput)
-      {params += 'inauthor:' + this.state.authorInput + '&';}
+      params += 'inauthor:' + this.state.authorInput + '&';
     if (this.state.categoryInput)
-      {console.log("cat param hit");
-      params += 'subject' + this.state.categoryInput + '&';}
-    console.log('https://www.googleapis.com/books/v1/volumes?q=' + params + maxAndStart + APIkey + fields);
+      params += 'subject' + this.state.categoryInput + '&';
     
     fetch('https://www.googleapis.com/books/v1/volumes?q=' + params + maxAndStart + APIkey + fields)
       .then(res => res.json())
         .then(data => {
-          const search = data.items;
-          this.setState({results: search});
+          this.setState({results: data.items, totalItems: data.totalItems});
         });  
+  }
+  
+  pageHandler = (pageIndex) => {
+    this.setState({page: pageIndex});
+    this.searchHandler();
   }
   
   render() {
@@ -106,6 +107,10 @@ class APITable extends Component {
           columns={columns}
           data={results}
           className='-striped -highlight'
+          manual
+          pages={Math.ceil(this.state.totalItems/20)}
+          page={this.state.page}
+          onPageChange={(pageIndex) => this.pageHandler(pageIndex)}
         />
       </div>
     );
